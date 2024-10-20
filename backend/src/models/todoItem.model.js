f3bcbb92888d336fdf523f66b1be0644def444f4
Milan Patel <milanpatel2851@gmail.com>
2024-10-20 15:39:51 +0530

    Changes in TodoItem model

--- backend/src/models/todoItem.model.js
+++ backend/src/models/todoItem.model.js
@@ -25,40 +25,8 @@ const todoSchema = new mongoose.Schema({
     default: false
   }
 }, {
-  timestamps: true,
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }
+  timestamps: true
 });
-
-// Indexes for improved query performance
-todoSchema.index({ user: 1, completed: 1 });
-todoSchema.index({ user: 1, createdAt: -1 });
-
-// Virtual for truncated description
-todoSchema.virtual('shortDescription').get(function() {
-  if (this.description) {
-    return this.description.length > 50 
-      ? `${this.description.substring(0, 50)}...` 
-      : this.description;
-  }
-  return '';
-});
-
-// Static method to get todos for a specific user
-todoSchema.statics.findByUser = function(userId) {
-  return this.find({ user: userId }).sort({ createdAt: -1 });
-};
-
-// Static method to get completed todos for a specific user
-todoSchema.statics.findCompletedByUser = function(userId) {
-  return this.find({ user: userId, completed: true }).sort({ createdAt: -1 });
-};
-
-// Instance method to toggle completion status
-todoSchema.methods.toggleComplete = async function() {
-  this.completed = !this.completed;
-  return this.save();
-};
 
 const Todo = mongoose.model('Todo', todoSchema);
 
